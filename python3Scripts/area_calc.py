1576c9bfe9136e71fa69978fe3e698e1ba6671de
Jose C Santos <minotaur423@comcast.net>
2018-09-14 11:30:23 -0400

    Fixed the problem with python3Scripts directory.

---
+++ python3Scripts/area_calc.py
@@ -0,0 +1,10 @@
+# hello
+# name = input('Tell me your name, punk: ')
+# age = input('...and your age: ')
+# print (name, 'you are', age)
+#print('Hello ' + name)
+
+# Calculate the area of a circle
+radius = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(radius)**2
+print(f'The area of your circle is: {area}')
