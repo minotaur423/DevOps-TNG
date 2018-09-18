922e380dab65c8c057f793dae8982d66d86d3ab0
minotaur423@me.com <nycadm@DevOps.example.com>
2018-09-18 13:44:39 -0400

    Wrapped up the Python tutorial today.  Searching for a new one.

---
+++ Lessons/decorators.py
@@ -0,0 +1,22 @@
+# Practice with decorators.
+def cough_dec(func):
+
+    def func_wrapper():
+        # Code before the function runs
+        print('*cough*')
+        func()
+        # Code after the function runs
+        print('*cough*')
+
+    return func_wrapper
+
+@cough_dec
+def question():
+    print('Can you give me a discount on that?')
+
+@cough_dec
+def answer():
+    print("It's only 50p you cheapskate.")
+
+question()
+answer()
---
+++ Lessons/lambdas.py
@@ -0,0 +1,10 @@
+# Practice with lambdas
+nums = [1,2,3,4,5,6]
+
+# The function shown below is replaced by the lambda in
+# the print statement
+
+# def square(n):
+#     return n * n
+
+print(list(map(lambda n: n * n,nums)))
